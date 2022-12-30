27cc04a2e76f5093557c70d8007f1e607589ab49
Yaroslav <biomerty@gmail.com>
2022-12-30 16:33:18 +0200

    Add project files.

---
+++ Failcodes/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Failcodes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(-1, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(336, 329);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(4, 334);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 18);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(254, 334);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = " STATUS:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FAILFUCK.Properties.Resources.red;
+            this.pictureBox1.Location = new System.Drawing.Point(313, 337);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(12, 12);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(334, 354);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FAILFUCK";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Failcodes/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System.Threading;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager;
+using System.Media;
+
+namespace Failcodes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += Form_FormClosing;
+            timer1.Tick += timer1_Tick;
+            this.Icon = FAILFUCK.Properties.Resources.coupon;
+            this.Text = "FAILFUCK v2";
+        }
+
+        EdgeDriver driver;
+        SoundPlayer player;
+        String promo_code = "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var options = new EdgeOptions();
+
+            options.BinaryLocation = @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe";
+            new DriverManager().SetUpDriver(new EdgeConfig());
+
+            options.AddUserProfilePreference("profile.default_content_setting_values.images", 2);
+            options.AddUserProfilePreference("profile.default_content_setting_values.stylesheets", 2);
+            options.AddArgument("--no-default-browser-check");
+            options.AddArgument("start-maximized");
+            options.AddArgument("disable-infobars");
+            options.AddArgument("--disable-extensions");
+            options.AddArgument("--no-sandbox");
+            options.AddArgument("--disable-application-cache");
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("--disable-dev-shm-usage");
+
+            var edgeDriverService = EdgeDriverService.CreateDefaultService();
+            edgeDriverService.HideCommandPromptWindow = true;
+            driver = new EdgeDriver(edgeDriverService, options);
+            driver.Manage().Window.Size = new Size(400, 800);
+            driver.Manage().Window.Minimize();
+            try
+            {
+                
+                driver.Url = "https://csfail.org/uk/bonuses";
+                Thread.Sleep(1000);
+                driver.FindElement(By.XPath("/html/body/app-root/shell-wrapper/shell-handheld-toolbar/div/div[1]/button[2]")).Click();
+                richTextBox1.Text += DateTime.Now.ToString("HH:mm") + " Завантажено. Очікування промокодів.";
+                timer1.Start();
+            }
+            catch { }
+
+            
+        }
+
+        void Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+                driver.Quit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                var uses = driver.FindElements(By.ClassName("promo__uses"));
+                var code = driver.FindElements(By.ClassName("promo__code"));
+                var type = driver.FindElements(By.ClassName("promo__type"));
+                if (code.Any() && type.Any())
+                {
+                    if (code.Last().Text.Length > 1 && type.Last().Text.Length > 1)
+                    {
+                        if (promo_code != code.Last().Text)
+                        {
+                            promo_code = code.Last().Text;
+                            Clipboard.SetText(promo_code);
+                            if (type.Last().Text.ToLower().Contains("обичка"))
+                                player = new SoundPlayer(FAILFUCK.Properties.Resources.miui_12_notification);
+                            if (type.Last().Text.ToLower().Contains("спец"))
+                                player = new SoundPlayer(FAILFUCK.Properties.Resources.iphone_notification);
+
+                            player.Play();
+                            richTextBox1.Text += "\n" + DateTime.Now.ToString("HH:mm") + " " + promo_code + " (" + type.Last().Text + ") " + uses.Last().Text;
+                        }
+                    }
+                }
+                var ping = driver.FindElement(By.ClassName("online__title"));
+                var online = driver.FindElement(By.ClassName("online__count"));
+                label1.Text = ping.Text + ", ONLINE " + online.Text;
+
+                var message = driver.FindElements(By.ClassName("message__text"));
+                if(message.Any())
+                {
+                    if(pictureBox1.Image != FAILFUCK.Properties.Resources.green)
+                    {
+                        pictureBox1.Image = FAILFUCK.Properties.Resources.green;
+                    }
+                }
+                else
+                {
+                    if (pictureBox1.Image != FAILFUCK.Properties.Resources.red)
+                    {
+                        pictureBox1.Image = FAILFUCK.Properties.Resources.red;
+                    }
+                }
+            }
+            catch { 
+            }
+        }
+
+    }
+}
